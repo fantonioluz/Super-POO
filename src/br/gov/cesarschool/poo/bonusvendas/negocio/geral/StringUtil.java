65332fdacde7519211477e867bf916a5d28876a5
camilacirne <camilacirne@hotmail.com>
2023-10-08 15:17:19 -0300

    primeiro madiator, desculpas chico, fui hipocrita

--- src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.java
+++ src/br/gov/cesarschool/poo/bonusvendas/negocio/geral/StringUtil.java
@@ -1,5 +1,15 @@
 package br.gov.cesarschool.poo.bonusvendas.negocio.geral;
 
 public class StringUtil {
-	
+
+    private StringUtil() {
+        }
+
+    public static boolean ehNuloOuBranco(String str){
+         String strSemEspaco = str.trim();
+        if(str == null || strSemEspaco.isEmpty()){
+            return true;
+        }
+
+    }
 }
