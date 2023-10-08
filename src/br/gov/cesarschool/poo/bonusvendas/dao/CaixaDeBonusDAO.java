94192d75e67408a30bdebdac7ab5bde58ec47dad
Francisco Antonio da Luz Filho <falf@cesar.school>
2023-10-08 12:10:25 -0300

    att

--- src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
@@ -1,6 +1,45 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
+import java.io.Serializable;
+import br.gov.cesarschool.poo.bonusvendas.entidade.CaixaDeBonus;
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+
 
 public class CaixaDeBonusDAO {
-    private 
-    
+    private static final String ARQUIVO = "";
+    private CadastroObjetos cadastro = new CadastroObjetos(CaixaDeBonus.class);
+
+    public boolean incluir(CaixaDeBonus caixaDeBonus) {
+        CaixaDeBonus caixaDeBonusExistente = buscar(caixaDeBonus.getNumero());
+        if (caixaDeBonusExistente != null) {
+            return false;
+        }
+        else{
+            cadastro.incluir(caixaDeBonus, ARQUIVO + caixaDeBonus.getNumero());
+            return true;
+        }
+    }
+
+    public boolean alterar(CaixaDeBonus caixaDeBonus) {
+        if (caixaDeBonus == null) {
+            return false;
+        }
+        else{
+            cadastro.alterar(caixaDeBonus, ARQUIVO + caixaDeBonus.getNumero());
+            return true;
+        }
+    }
+
+    public boolean excluir(CaixaDeBonus caixaDeBonus) {
+        if (caixaDeBonus == null) {
+            return false;
+        }
+        else{
+            cadastro.excluir(ARQUIVO + caixaDeBonus.getNumero());
+            return true;
+        }
+    }
+
+    public CaixaDeBonus buscar(long numero) {
+        return (CaixaDeBonus) cadastro.buscar(ARQUIVO + numero);
+    }
 }
