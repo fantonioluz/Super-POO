d1541bdfc5fc685bfa389036b0042e155795f8a1
Francisco Antonio da Luz Filho <falf@cesar.school>
2023-10-09 09:29:39 -0300

    Create ResultadoInclusaoVendedor.java

---
+++ src/br/gov/cesarschool/poo/bonusvendas/negocio/ResultadoInclusaoVendedor.java
@@ -0,0 +1,26 @@
+package br.gov.cesarschool.poo.bonusvendas.negocio;
+
+public class ResultadoInclusaoVendedor {
+
+    private long numeroCaixaDeBonus;
+    private String mensagemErroValidacao;
+
+
+    public ResultadoInclusaoVendedor(long numeroCaixaDeBonus, String mensagemErroValidacao) {
+        this.numeroCaixaDeBonus = numeroCaixaDeBonus;
+        this.mensagemErroValidacao = mensagemErroValidacao;
+    }
+
+
+    public long getNumeroCaixaDeBonus() {
+        return numeroCaixaDeBonus;
+    }
+
+
+    public String getMensagemErroValidacao() {
+        return mensagemErroValidacao;
+    }
+
+    
+    
+}
