8f2420dc0de763332537075fe7621213fefdf89a
Francisco Antonio da Luz Filho <falf@cesar.school>
2023-10-12 15:50:40 -0300

    Update TelaAcumuloResgate.java

--- src/br/gov/cesarschool/poo/bonusvendas/tela/TelaAcumuloResgate.java
+++ src/br/gov/cesarschool/poo/bonusvendas/tela/TelaAcumuloResgate.java
@@ -1,5 +1,7 @@
 package br.gov.cesarschool.poo.bonusvendas.tela;
 
+import javax.swing.JOptionPane;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -9,7 +11,11 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
-import br.gov.cesarschool.poo.bonusvendas.negocio.AcumuloResgateMediator;
+
+import br.gov.cesarschool.poo.bonusvendas.dao.CaixaDeBonusDAO;
+import br.gov.cesarschool.poo.bonusvendas.entidade.CaixaDeBonus;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 
 
@@ -20,17 +26,18 @@ public class TelaAcumuloResgate {
 	
 	protected Shell shell;
 	
-	private AcumuloResgateMediator mediator = AcumuloResgateMediator.getInstancia();
+	private CaixaDeBonusDAO mediator = new CaixaDeBonusDAO();
 
 
 	private Text NumeroCaixa;
 	private Text Saldo;
-	private Label lblNewLabel;
-	private Label lblNewLabel_1;
-	private Label lblNewLabel_2;
-	private Label lblNewLabel_3;
-	private Label lblNewLabel_4;
 	private Text valor;
+	private Button Acumular;
+	private Button Resgatar;
+	private Combo tipoResgate;
+	private Button acumularOuResgatar;
+	private Button voltar;
+	private Button Buscar;
 
 
 
@@ -75,10 +82,12 @@ public class TelaAcumuloResgate {
 		NumeroCaixa.setBounds(155, 73, 91, 18);
 		
 		Button Acumular = new Button(shell, SWT.RADIO);
+		this.Acumular = Acumular;
 		Acumular.setBounds(21, 119, 77, 15);
 		Acumular.setText("Acumular");
 		
 		Button Resgatar = new Button(shell, SWT.RADIO);
+		this.Resgatar = Resgatar;
 		Resgatar.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -88,29 +97,91 @@ public class TelaAcumuloResgate {
 		Resgatar.setText("Regastar");
 		
 		Saldo = new Text(shell, SWT.BORDER);
+		Saldo.setEnabled(false);
 		Saldo.setEditable(false);
-		Saldo.setBounds(322, 72, 59, 18);
+		Saldo.setBounds(322, 73, 59, 18);
 		
 		Label label = new Label(shell, SWT.SEPARATOR | SWT.VERTICAL);
 		label.setBounds(264, 73, 1, 146);
 		
 		
 		Combo tipoResgate = new Combo(shell, SWT.NONE);
+		tipoResgate.setEnabled(false);
+		this.tipoResgate = tipoResgate;
 		tipoResgate.setItems(new String[] {"Serviço", "Produto", "Cash"});
 		tipoResgate.setBounds(285, 115, 96, 19);
 		tipoResgate.setText("Tipo de resgate");
 		
 		valor = new Text(shell, SWT.BORDER);
+		valor.setEnabled(false);
 		valor.setBounds(322, 152, 41, 18);
 		
 		Button acumularOuResgatar = new Button(shell, SWT.BORDER);
+		acumularOuResgatar.setEnabled(false);
+		this.acumularOuResgatar = acumularOuResgatar;
 		acumularOuResgatar.setBounds(285, 194, 96, 20);
 		acumularOuResgatar.setText("Acumular/Resgatar");
 		
 		Button voltar = new Button(shell, SWT.BORDER);
+		voltar.setEnabled(false);
+		this.voltar = voltar;
 		voltar.setBounds(285, 220, 61, 20);
 		voltar.setText("Voltar");
+		
+		Label lblNewLabel = new Label(shell, SWT.NONE);
+		lblNewLabel.setBounds(24, 76, 128, 15);
+		lblNewLabel.setText("Numero da Caixa de Bonus");
+		
+		Label lblNewLabel_1 = new Label(shell, SWT.NONE);
+		lblNewLabel_1.setBounds(24, 100, 49, 15);
+		lblNewLabel_1.setText("Operação");
+		
+		Label lblSaldo = new Label(shell, SWT.NONE);
+		lblSaldo.setEnabled(false);
+		lblSaldo.setBounds(283, 75, 33, 15);
+		lblSaldo.setText("Saldo");
+		
+		Label lblNewLabel_2 = new Label(shell, SWT.NONE);
+		lblNewLabel_2.setEnabled(false);
+		lblNewLabel_2.setBounds(283, 154, 33, 15);
+		lblNewLabel_2.setText("Valor");
+		
+		Button Buscar = new Button(shell, SWT.BORDER);
+		Buscar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				CaixaDeBonus caixaDeBonus = mediator.buscar(Long.parseLong(NumeroCaixa.getText()));
+				if(caixaDeBonus == null) {
+					JOptionPane.showMessageDialog(null, 
+					"Caixa de bonus inexistente!");
+				} else {
+					Saldo.setText(String.valueOf(caixaDeBonus.getSaldo()));
+					Saldo.setEnabled(true);
+					lblSaldo.setEnabled(true);
+					if(Acumular.getSelection()) {
+						valor.setEnabled(true);
+						acumularOuResgatar.setEnabled(true);
+						voltar.setEnabled(true);
+					} else if(Resgatar.getSelection()) {
+						tipoResgate.setEnabled(true);
+						valor.setEnabled(true);
+						acumularOuResgatar.setEnabled(true);
+						voltar.setEnabled(true);
+					}
+				}
+				
+
+
+			}
+		});
+		this.Buscar = Buscar;
+		Buscar.setBounds(21, 194, 61, 20);
+		Buscar.setText("Buscar");
 
 
 	}
+
+
+
+	
 }
