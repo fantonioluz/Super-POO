a6fbc0767d6c1b97f474ad437301ac2d2b88454a
Gislaine Reis <gmvbr.2002@gmail.com>
2023-11-27 17:34:39 -0300

    atualizando o DAO de lancamento de bonus

--- src/br/gov/cesarschool/poo/bonusvendas/dao/LancamentoBonusDAO.java
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/LancamentoBonusDAO.java
@@ -1,13 +1,16 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
 
-import java.io.Serializable;
 import java.time.format.DateTimeFormatter;
 import br.gov.cesarschool.poo.bonusvendas.entidade.LancamentoBonus;
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+import br.edu.cesarschool.next.oo.persistenciaobjetos.DAOGenerico;
 
 public class LancamentoBonusDAO {
     private static final String ARQUIVO = "";
-    private CadastroObjetos cadastro = new CadastroObjetos(LancamentoBonus.class);
+    private DAOGenerico<LancamentoBonus> dao;
+
+    public LancamentoBonusDAO() {
+        this.dao = new DAOGenerico<>(LancamentoBonus.class);
+    }
 
     public String idLancamento(LancamentoBonus lancamentoBonus) {
         String dataString = lancamentoBonus.getDataHoraLancamento().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
@@ -22,7 +25,7 @@ public class LancamentoBonusDAO {
         if (buscaLancamentoObj != null) {
             return false;
         } else {
-            cadastro.incluir(lancamentoBonus, ARQUIVO + buscaLancamento);
+            dao.incluir(lancamentoBonus, ARQUIVO + buscaLancamento);
             return true;
         }
     }
@@ -34,7 +37,7 @@ public class LancamentoBonusDAO {
         if (buscaLancamentoObj == null) {
             return false;
         } else {
-            cadastro.alterar(lancamentoBonus, ARQUIVO + buscaLancamento);
+            dao.excluir(ARQUIVO + buscaLancamento);
             return true;
         }
     }
@@ -52,15 +55,10 @@ public class LancamentoBonusDAO {
     }
 
     public LancamentoBonus buscar(String idLancamento) {
-        return (LancamentoBonus) cadastro.buscar(ARQUIVO + idLancamento);
+        return dao.buscar(ARQUIVO + idLancamento);
     }
 
     public LancamentoBonus[] buscarTodos() {
-        Serializable[] rets = cadastro.buscarTodos(LancamentoBonus.class);
-        LancamentoBonus[] lancamentoBonus = new LancamentoBonus[rets.length];
-        for (int i = 0; i < rets.length; i++) {
-            lancamentoBonus[i] = (LancamentoBonus) rets[i];
-        }
-        return lancamentoBonus;
+        return dao.buscarTodos();
     }
 }
