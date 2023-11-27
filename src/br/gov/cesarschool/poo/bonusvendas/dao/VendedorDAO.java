c97c09072c985ec34f544c8152ddfd474218cf91
Gislaine Reis <gmvbr.2002@gmail.com>
2023-11-27 17:34:50 -0300

    atualizando o DAO do vendedor

--- src/br/gov/cesarschool/poo/bonusvendas/dao/VendedorDAO.java
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/VendedorDAO.java
@@ -1,20 +1,22 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
 
-import java.io.Serializable;
-
 import br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
-import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+import br.edu.cesarschool.next.oo.persistenciaobjetos.DAOGenerico;
 
 public class VendedorDAO {
     private static final String ARQUIVO = "";
-    private CadastroObjetos cadastro = new CadastroObjetos(Vendedor.class);
+    private DAOGenerico<Vendedor> dao;
+
+    public VendedorDAO() {
+        this.dao = new DAOGenerico<>(Vendedor.class);
+    }
 
     public boolean incluir(Vendedor vendedor) {
         Vendedor buscaVendedor = buscar(vendedor.getCpf());
         if(buscaVendedor != null){
             return false;
         } else {
-            cadastro.incluir(vendedor, ARQUIVO + vendedor.getCpf());
+            dao.incluir(vendedor, ARQUIVO + vendedor.getCpf());
             return true;
         }
     }
@@ -24,7 +26,7 @@ public class VendedorDAO {
         if(buscaVendedor == null){
             return false;
         } else {
-            cadastro.alterar(vendedor, ARQUIVO + vendedor.getCpf());
+            dao.alterar(vendedor, ARQUIVO + vendedor.getCpf());
             return true;
         }
     }
@@ -34,21 +36,16 @@ public class VendedorDAO {
         if(buscaVendedor == null){
             return false;
         } else {
-            cadastro.excluir(ARQUIVO + vendedor.getCpf());
+            dao.excluir(ARQUIVO + vendedor.getCpf());
             return true;
         }
     }
 
     public Vendedor buscar(String cpf) {
-        return (Vendedor)cadastro.buscar(ARQUIVO + cpf);
+        return dao.buscar(ARQUIVO + cpf);
     }
 
     public Vendedor[] buscarTodos() {
-        Serializable[] rets = cadastro.buscarTodos(Vendedor.class);
-        Vendedor[] vendedor = new Vendedor[rets.length];
-        for(int i = 0; i<rets.length; i++) {
-            vendedor[i] = (Vendedor)rets[i];
-        }
-        return vendedor;
+        return dao.buscarTodos();
     }
 }
